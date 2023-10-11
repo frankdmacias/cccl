15d76304fc351ce4184865f47a49bc1c3ab31645
Michael Schellenberger Costa <miscco@nvidia.com>
2023-10-11 09:02:20 +0200

    Move `std::mismatch` into its own file

--- libcudacxx/libcxx/test/std/algorithms/alg.nonmodifying/mismatch/mismatch.pass.cpp
+++ libcudacxx/libcxx/test/std/algorithms/alg.nonmodifying/mismatch/mismatch.pass.cpp
@@ -23,6 +23,10 @@
 #include "test_macros.h"
 #include "test_iterators.h"
 
+#ifdef TEST_COMPILER_MSVC_2017
+#pragma warning(disable: 4018) // signed/unsigned mismatch
+#endif // TEST_COMPILER_MSVC_2017
+
 #if TEST_STD_VER > 17
 TEST_CONSTEXPR bool test_constexpr() {
     int ia[] = {1, 3, 6, 7};
